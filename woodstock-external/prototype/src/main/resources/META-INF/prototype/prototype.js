4d48b4653872e07eceea9c993b49f1dc76a2d22d
NotedSalmon <moreiraandre2001@gmail.com>
2025-12-18 19:07:20 +0000

    fixed copyright?

--- woodstock-external/prototype/src/main/resources/META-INF/prototype/prototype.js
+++ woodstock-external/prototype/src/main/resources/META-INF/prototype/prototype.js
@@ -4,7 +4,22 @@
  *  Prototype is freely distributable under the terms of an MIT-style license.
  *  For details, see the Prototype web site: http://www.prototypejs.org/
  *
- *--------------------------------------------------------------------------*/
+ *--------------------------------------------------------------------------
+ *  Copyright (c) 2022, 2025 Contributors to the Eclipse Foundation. All rights reserved.
+    Copyright (c) 2025, 2020 Oracle and/or its affiliates. All rights reserved.
+
+    This program and the accompanying materials are made available under the
+    terms of the Eclipse Public License v. 2.0, which is available at
+    http://www.eclipse.org/legal/epl-2.0.
+
+    This Source Code may also be made available under the following Secondary
+    Licenses when the conditions for such availability set forth in the
+    Eclipse Public License v. 2.0 are satisfied: GNU General Public License,
+    version 2 with the GNU Classpath Exception, which is available at
+    https://www.gnu.org/software/classpath/license.html.
+
+    SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ * */
 
 var Prototype = {
 
@@ -4532,16 +4547,6 @@ Prototype._original_property = window.Sizzle;
   }
 })();
 
-/*!
- * Sizzle CSS Selector Engine v1.10.18
- * http://sizzlejs.com/
- *
- * Copyright 2013 jQuery Foundation, Inc. and other contributors
- * Released under the MIT license
- * http://jquery.org/license
- *
- * Date: 2014-02-05
- */
 (function( window ) {
 
 var i,
